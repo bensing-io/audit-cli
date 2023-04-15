f64fa5f495b406518883ff6686122b3d1cf1eff5
Bill Bensing <billbensing@bills-mbp.lan>
2023-04-14 21:55:59 -0400

    moved audit logic into it's own file

--- cli.py
+++ cli.py
@@ -2,10 +2,15 @@ import argparse
 
 from audit import Audit
 
-if __name__ == '__main__':
+
+def parse_arguments(self):
     parser = argparse.ArgumentParser(description='CLI to evaluate a file against a set of procedures')
     parser.add_argument('-f', '--file', required=True, help='file to evaluate')
     parser.add_argument('-p', '--procedures', required=True, help='directory containing the audit procedure files')
     parser.add_argument('-o', '--out', required=False, help='directory to output JSON report')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
     Audit(args).run()
