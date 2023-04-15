417965bac86350a825efee89e193ebaf83c86272
Bill Bensing <billbensing@bills-mbp.lan>
2023-04-14 21:57:44 -0400

    cleaned up cli

--- cli.py
+++ cli.py
@@ -3,14 +3,10 @@ import argparse
 from audit import Audit
 
 
-def parse_arguments(self):
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CLI to evaluate a file against a set of procedures')
     parser.add_argument('-f', '--file', required=True, help='file to evaluate')
     parser.add_argument('-p', '--procedures', required=True, help='directory containing the audit procedure files')
     parser.add_argument('-o', '--out', required=False, help='directory to output JSON report')
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    args = parse_arguments()
+    args = parser.parse_args()
     Audit(args).run()
